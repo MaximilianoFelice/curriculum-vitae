0f03463480eb8a5b1d0b07fec242aa2280c2372a
Maximiliano Felice <maximilianofelice@gmail.com>
2015-12-22 01:10:19 -0300

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  #
+  # => Validations
+  #
+  validates :first_name, :last_name, :birthdate, :address, :administrative_area_level_1, :country, :national_id, :phone, :email, :description, presence: true
 end

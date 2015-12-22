4cbbdc1e6f4f401daae9fb64be1458ca26cd5312
Maximiliano Felice <maximilianofelice@gmail.com>
2015-12-21 23:55:17 -0300

    Added godmin basicauth

--- admin/app/controllers/admin/application_controller.rb
+++ admin/app/controllers/admin/application_controller.rb
@@ -1,5 +1,7 @@
 module Admin
   class ApplicationController < ActionController::Base
+    http_basic_authenticate_with name: Rails.application.secrets.godmin['user'], password: Rails.application.secrets.godmin['pass']
+
     include Godmin::ApplicationController
   end
 end

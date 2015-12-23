c6a0caadac1efb71706b222ce24a8bd8722e2b5a
Maximiliano Felice <maximilianofelice@gmail.com>
2015-12-23 04:05:50 -0300

    Fixed animation stepping over width

--- app/assets/javascripts/progress_bar_animation.js
+++ app/assets/javascripts/progress_bar_animation.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   $(".meter > span").each(function() {
     $(this)
-      .data("origWidth", $(this).width())
+      .data("origWidth", $(this).data('widthPercentage'))
       .width(0)
       .animate({
-        width: $(this).data("origWidth") // or + "%" if fluid
+        width: $(this).data("origWidth") + "%"
       }, 1200);
   });
 })

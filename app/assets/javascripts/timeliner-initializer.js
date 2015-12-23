ac8a800c5e7333e999c3c45b9f45f63288cd7f87
Maximiliano Felice <maximilianofelice@gmail.com>
2015-12-22 23:16:05 -0300

    Added timlining style to title

--- app/assets/javascripts/timeliner-initializer.js
+++ app/assets/javascripts/timeliner-initializer.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $('.timeline-container').each(function(index, element){ 
-    $.timeliner({timelineContainer: '#' + $(element).prop('id'),startState: 'open', fontOpen: '1em', fontClosed: '1em'}) 
+    $.timeliner({timelineContainer: '#' + $(element).prop('id'),startState: 'open', fontOpen: '0.75em', fontClosed: '0.75em'}) 
   });
 })

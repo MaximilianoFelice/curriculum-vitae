c583639544dba1dbbe69fbb0ebbfd4f245d7bcff
Maximiliano Felice <maximilianofelice@gmail.com>
2015-12-29 17:51:46 -0300

    Updated pfd css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     @user = User.first.decorate
     respond_to do |format|
       format.html
-      format.pdf { render pdf: "MaximilianoFelice", disposition: 'inline', layout: "layouts/pdf.html", show_as_html: params[:debug].present?, formats: [:html], encoding: 'UTF-8' }
+      format.pdf { render pdf: "MaximilianoFelice", disposition: 'inline', layout: "layouts/pdf.html", show_as_html: params[:debug].present?, formats: [:html], encoding: 'UTF-8', viewport_size: '1280x1024'}
     end
   end
 

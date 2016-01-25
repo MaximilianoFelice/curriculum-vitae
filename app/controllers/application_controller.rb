22ae25acbd32f311cf4aa38ce34be7ceba7d73dc
Maximiliano Felice <maximilianofelice@gmail.com>
2016-01-25 03:02:11 -0300

    Updated user tracking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,23 @@
 class ApplicationController < ActionController::Base
   # MixPanel Tracking info #
   include Mengpaneel::Controller
-  
+
+  # Setup MixPanel
+  before_filter :setup_mixpanel
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def is_dev_mode?
     !Rails.env.production? || CloudFlare.dev_mode?
+  end
+
+  private
+  def setup_mixpanel
+    session[:user] = SecureRandom.hex unless session[:user]
+    mengpaneel.setup do
+      mixpanel.identify(session[:user])
+    end
   end
 end
